a0bdc4af590114a11cb9067f2d7272e3eaebf7cb
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-12 23:34:29 +0000

    add secret property to trials

--- db/migrate/20170812222610_create_trials.rb
+++ db/migrate/20170812222610_create_trials.rb
@@ -3,6 +3,7 @@ class CreateTrials < ActiveRecord::Migration[5.1]
     create_table :trials do |t|
       t.string :title
       t.text :description
+      t.boolean :secret, default: false
 
       t.timestamps
     end
