7b5f0893ca7da7fb70be1c33cf67e8d852a62e5c
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-12 21:52:45 +0000

    Configure test suite and add user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name 'John'
+    last_name 'Doe'
+    email { Faker::Internet.email }
+    password 'password'
+    password_confirmation 'password'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_uniqueness_of :email }
+    it { is_expected.to validate_confirmation_of :password }
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+  end
+
+  context 'instance methods' do
+
+
+
+  end
 end
