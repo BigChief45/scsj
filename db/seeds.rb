71d9beb6c5200b65873b23edb3e0d4a787a09826
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-12 23:34:09 +0000

    add trial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(first_name: 'John', last_name: 'Doe', email: 'johndoe@gmail.com',
+  password: '12345678', password_confirmation: '12345678')
+
+
+# Create fake trials
+10.times do
+  Trial.create(
+    title: 'Lorem Ipsum...',
+    description: 'Blah blah blah blah'
+  )
+end
