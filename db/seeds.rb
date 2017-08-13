a53be1356194292d463a5f2247d287f6504f53f9
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-13 17:40:24 +0000

    Add more user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
-admin = User.create(first_name: 'John', last_name: 'Doe', email: 'johndoe@gmail.com',
-  password: '12345678', password_confirmation: '12345678')
+admin = User.create(first_name: 'John', last_name: 'Doe',
+  email: 'johndoe@gmail.com', password: '12345678',
+  password_confirmation: '12345678')
 admin.add_role :admin
+
+judge = User.create(first_name: 'Jamie', last_name: 'Lannister',
+  email: 'jlan@gmail.com', password: '12345678',
+  password_confirmation: '12345678')
+judge.add_role :judge
+
+lawyer = User.create(first_name: 'Robert', last_name: 'Baratheon',
+  email: 'rbarat@gmail.com', password: '12345678',
+  password_confirmation: '12345678')
+lawyer.add_role :lawyer
 
 # Create fake trials
 10.times do
