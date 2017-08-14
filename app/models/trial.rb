d3a0477c6d9374ec2b63b4468efd237a5f8725b5
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-14 03:12:19 +0000

    Add associations between Trial and specific person classes

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -3,4 +3,8 @@ class Trial < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :start_date, presence: true
+
+  has_many :trial_people
+  has_many :plaintiffs, class_name: 'Plaintiff', through: :trial_people, source: :person
+  has_many :defendants, class_name: 'Defendant', through: :trial_people, source: :person
 end
