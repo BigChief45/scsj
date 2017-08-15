16565d7cc04af2800655370210008a4ddf273618
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-15 17:57:50 +0000

    update specs

--- spec/models/trial_person_spec.rb
+++ spec/models/trial_person_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe TrialPerson, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'associations' do
+    it { is_expected.to belong_to :trial }
+    it { is_expected.to belong_to :triable }
+  end
+
 end
--- spec/models/trial_spec.rb
+++ spec/models/trial_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe Trial, type: :model do
     it { is_expected.to validate_presence_of :start_date }
   end
 
+  context 'associations' do
+    it { is_expected.to have_many :trial_people }
+
+    it { is_expected.to have_many(:plaintiffs).through(:trial_people).source(:triable) }
+    it { is_expected.to have_many(:defendants).through(:trial_people).source(:triable) }
+  end
+
 end
