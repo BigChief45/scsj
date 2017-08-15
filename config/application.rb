eac115901d5ec3d7db658f0bea6aaa7bad20eaa6
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-15 18:05:30 +0000

    Fix :en locale issue

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Scsj
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.available_locales = :es
+    config.i18n.available_locales = [:es, :en]
     config.i18n.default_locale = :es
 
      # Layouts for Devise views
