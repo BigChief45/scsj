e114b8f5203c245744d20912475896b95328bccb
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-28 23:21:29 +0000

    Improved seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,51 +4,62 @@ Role.create!(name: 'admin')
 Role.create!(name: 'judge')
 Role.create!(name: 'lawyer')
 
-admin = User.create!(first_name: 'John', last_name: 'Doe',
-  mobile_number: Faker::PhoneNumber.cell_phone,
-  email: 'admin@admin.com', password: '12345678',
-  password_confirmation: '12345678')
-admin.add_role :admin
+if Rails.env.development?
 
-
-# Create judges
-5.times do
-  judge = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
-    email: Faker::Internet.email, password: '12345678',
+  admin = User.create!(first_name: 'John', last_name: 'Doe',
     mobile_number: Faker::PhoneNumber.cell_phone,
+    email: 'admin@admin.com', password: '12345678',
     password_confirmation: '12345678')
-  judge.add_role :judge
-end
-
-# Create lawyers
-20.times do
-  lawyer = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
-    email: Faker::Internet.email , password: '12345678',
-    mobile_number: Faker::PhoneNumber.cell_phone,
-    password_confirmation: '12345678')
-  lawyer.add_role :lawyer
-end
+  admin.add_role :admin
 
 
-# People
-5.times do
-  Person.create!(name: Faker::Name.first_name, last_name: Faker::Name.last_name,
-    id_number: Faker::Number.number(12), birth_date: Date.today)
-end
+  # Create judges
+  5.times do
+    judge = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+      email: Faker::Internet.email, password: '12345678',
+      mobile_number: Faker::PhoneNumber.cell_phone,
+      password_confirmation: '12345678')
+    judge.add_role :judge
+  end
+
+  # Create lawyers
+  20.times do
+    lawyer = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+      email: Faker::Internet.email , password: '12345678',
+      mobile_number: Faker::PhoneNumber.cell_phone,
+      password_confirmation: '12345678')
+    lawyer.add_role :lawyer
+  end
 
 
-# Create fake trials
-10.times do
-  lawyers = User.lawyers.limit(2).order('RANDOM()')
-  Trial.create!(
-    title: Faker::Book.title,
-    description: Faker::Lorem.paragraph(10),
-    start_date: Date.today,
-    secret: [true, false].sample,
-    judge: User.judges.order('RANDOM()').limit(1).first,
-    plaintiffs_lawyer: lawyers.first,
-    defendants_lawyer: lawyers.last,
-    plaintiff_ids: Person.order('RANDOM()').limit(1).map { |p| p.id },
-    defendant_ids: Person.order('RANDOM()').limit(1).map { |p| p.id }
-  )
+  # People
+  5.times do
+    Person.create!(name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+      id_number: Faker::Number.number(12), birth_date: Date.today)
+  end
+
+
+  # Create fake trials
+  10.times do
+    lawyers = User.lawyers.limit(2).order('RANDOM()')
+    trial = Trial.create!(
+      title: Faker::Book.title,
+      description: Faker::Lorem.paragraph(10),
+      start_date: Date.today,
+      secret: [true, false].sample,
+      judge: User.judges.order('RANDOM()').limit(1).first,
+      plaintiffs_lawyer: lawyers.first,
+      defendants_lawyer: lawyers.last,
+      plaintiff_ids: Person.order('RANDOM()').limit(1).map { |p| p.id },
+      defendant_ids: Person.order('RANDOM()').limit(1).map { |p| p.id }
+    )
+
+    Random.rand(10).times do
+      trial.trial_presentations.create!(
+        description: Faker::Lorem.paragraph(Random.rand(20) + 5),
+        lawyer: [trial.plaintiffs_lawyer, trial.defendants_lawyer, trial.judge].sample
+        )
+    end
+  end
+
 end
