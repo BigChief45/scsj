593d1cd676f9fe3570c15baee0e6609fdee7697d
Jaime Alvarez <andresalvarez353@gmail.com>
2017-08-29 20:22:19 +0000

    Create notification when TrialPresentation is created

--- app/models/trial_presentation.rb
+++ app/models/trial_presentation.rb
@@ -1,8 +1,25 @@
 class TrialPresentation < ApplicationRecord
+  after_commit :create_notifications
+
   mount_uploaders :attachments, AttachmentUploader
 
   validates :description, presence: true
 
-  belongs_to :user#:lawyer, class_name: 'User', foreign_key: :lawyer_id
   belongs_to :trial
+  belongs_to :user
+
+  private
+
+  def recipients
+    [self.trial.judge, self.trial.plaintiffs_lawyer, self.trial.defendants_lawyer] - [self.user]
+  end
+
+  def create_notifications
+    # Create notifications for judge and lawyers
+
+    recipients.each do |recipient|
+      Notification.create(recipient: recipient, actor: self.user,
+        action: 'posted', notifiable: self)
+    end
+  end
 end
