6fb3ab2ade001cf18558cca021c992473bd12556
Jaime Alvarez <andresalvarez353@gmail.com>
2017-09-04 21:54:49 +0000

    Refactor icon helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def render_icon(icon_name, options)
+    case options[:size]
+    when :big
+      image_tag("icons/32/#{icon_name}")
+    when :small
+      image_tag("icons/16/#{icon_name}")
+    end
+  end
+
 end
--- app/helpers/trial_presentations_helper.rb
+++ app/helpers/trial_presentations_helper.rb
@@ -1,5 +1,13 @@
 module TrialPresentationsHelper
 
+  def new_presentation_icon(options={})
+    render_icon('page_add.png', options)
+  end
+
+  def presentation_notification_icon(options={})
+    render_icon('document_quote.png', options)
+  end
+
   def attachment_icon(identifier)
     case identifier.split('.').last
     when 'txt', 'doc', 'pdf'
@@ -10,4 +18,5 @@ module TrialPresentationsHelper
       ''
     end
   end
+
 end
