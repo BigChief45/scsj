7301a211a55da1fe0b9c2b6c4d7b84561670e663
Jaime Alvarez <andresalvarez353@gmail.com>
2017-09-07 22:32:15 +0000

    Mailer fixes

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,8 @@ class NotificationMailer < ApplicationMailer
   def notification_email(recipient, notification)
     @recipient = recipient
     @notification = notification
+
+    # TODO: Fix URL for all notifiable types
     @url = trial_url(notification.notifiable)
 
     mail(to: @recipient.email, subject: t('email.notifications.subject'))
