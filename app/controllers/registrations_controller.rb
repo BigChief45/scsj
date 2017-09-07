cc1e44d5a84d3530af65451098a90542f0dfd508
Jaime Alvarez <andresalvarez353@gmail.com>
2017-09-07 22:38:10 +0000

    Fix account update strong param bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -47,8 +47,8 @@ class RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, :mobile_number,
-      keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:mobile_number,
+      :avatar])
   end
 
   # The path used after sign up.
